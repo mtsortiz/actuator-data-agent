2eb5c1f1916c31082518542a8984852551fcdd71
Matias <matiasnortiz.dev@gmail.com>
2026-06-30 16:44:37 -0300

    agent finished

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -8,39 +8,20 @@ from langgraph.checkpoint.memory import MemorySaver
 
 load_dotenv()
 
-# llm = ChatOpenAI(
-#     model="gpt-5-mini",
-#     temperature=0.0
-# )
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+llm = ChatOpenAI(
+    model="gpt-5-mini",
     temperature=0.0
 )
 
 memory = MemorySaver()
 
-# agent = create_agent(
-#     model=ChatOpenAI(
-#         model="gpt-5-mini",
-#         temperature=0.1,
-#     ),
-#     tools=tools,
-#     system_prompt = """
-# You are the Konecto Intelligent Actuator Data Agent. Your job is to assist clients 
-# in finding and specifying the correct actuator models from the Series 76 catalog.
-# Rules for your execution:
-# 1. You have tools to query the local SQLite database. Always use them when asked about specific technical data, parts, or specifications.
-# 2. If the user asks about a specific part number, call `query_by_part_number`.
-# 3. If the user provides physical requirements (voltage, phase, torque, speed, motor power), call `query_by_specs`.
-# 4. Be polite, precise, and reply based on the technical specifications retrieved.
-# """
-
 agent = create_agent(
-    model=llm,
+    model=ChatOpenAI(
+        model="gpt-5-mini",
+        temperature=0.1,
+    ),
     tools=tools,
-    checkpointer=memory,
-    system_prompt="""
+    system_prompt = """
 You are the Konecto Intelligent Actuator Data Agent. Your job is to assist clients 
 in finding and specifying the correct actuator models from the Series 76 catalog.
 Rules for your execution:
